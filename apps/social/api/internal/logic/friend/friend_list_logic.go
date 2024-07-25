c37ba2e3cc4562def87a8ed1fbbea7e8ce1c3a4b
xyl <416319808@qq.com>
2024-07-25 18:34:23 +0800

    好友列表

--- apps/social/api/internal/logic/friend/friend_list_logic.go
+++ apps/social/api/internal/logic/friend/friend_list_logic.go
@@ -2,6 +2,9 @@ package friend
 
 import (
 	"context"
+	"easy-chat/apps/social/rpc/social"
+	"easy-chat/apps/user/rpc/user"
+	"easy-chat/pkg/ctxdata"
 
 	"easy-chat/apps/social/api/internal/svc"
 	"easy-chat/apps/social/api/internal/types"
@@ -23,8 +26,53 @@ func NewFriendListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Friend
 	}
 }
 
+// FriendList 好友列表
 func (l *FriendListLogic) FriendList(req *types.FriendListReq) (resp *types.FriendListResp, err error) {
-	// todo: add your logic here and delete this line
+	uid := ctxdata.GetUId(l.ctx)
 
-	return
+	friends, err := l.svcCtx.SocialRpc.FriendList(l.ctx, &social.FriendListReq{
+		UserId: uid,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(friends.List) == 0 {
+		return &types.FriendListResp{}, nil
+	}
+
+	uids := make([]string, 0, len(friends.List))
+	for _, i := range friends.List {
+		uids = append(uids, i.FriendUid)
+	}
+
+	users, err := l.svcCtx.UserRpc.FindUser(l.ctx, &user.FindUserReq{
+		Ids: uids,
+	})
+	if err != nil {
+		return &types.FriendListResp{}, nil
+	}
+
+	userRecords := make(map[string]*user.UserEntity, len(users.User))
+	for i, _ := range users.User {
+		userRecords[users.User[i].Id] = users.User[i]
+	}
+
+	respList := make([]*types.Friends, 0, len(friends.List))
+	for _, v := range friends.List {
+		friend := &types.Friends{
+			Id:        v.Id,
+			FriendUid: v.FriendUid,
+		}
+
+		if u, ok := userRecords[v.FriendUid]; ok {
+			friend.Nickname = u.Nickname
+			friend.Avatar = u.Avatar
+		}
+		respList = append(respList, friend)
+	}
+
+	return &types.FriendListResp{
+		List: respList,
+	}, nil
 }
