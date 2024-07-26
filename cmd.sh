d501a56e25446e426d298e7d96416c521a376864
xyl <416319808@qq.com>
2024-07-26 23:48:27 +0800

    sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,7 @@
 goctl rpc protoc apps/social/rpc/social.proto --go_out=./apps/social/rpc --go-grpc_out=./apps/social/rpc --zrpc_out=./apps/social/rpc -style go_zero
 
 goctl api go -api apps/social/api/social.api -dir apps/social/api -style go_zero
+
+goctl model mysql ddl -src="./deploy/sql/user.sql" -dir="./apps/user/models/" -c -style go_zero
+
+goctl model mysql ddl -src="./deploy/sql/social.sql" -dir="./apps/social/socialModels/" -c
--- model.sh
+++
@@ -1,3 +0,0 @@
-goctl model mysql ddl -src="./deploy/sql/user.sql" -dir="./apps/user/models/" -c -style go_zero
-
-goctl model mysql ddl -src="./deploy/sql/social.sql" -dir="./apps/social/socialModels/" -c
